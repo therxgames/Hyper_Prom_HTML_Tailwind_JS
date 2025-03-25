b3b4d39dc2017c896d937842aec448a5cefa3d7b
rmihaylov <therxgames0@gmail.com>
2025-03-25 22:51:50 +0300

    Верстка Главной О нас Каталог Категория

---
+++ js/tailwind.config.js
@@ -0,0 +1,34 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      boxShadow: {
+        header: '0 4px 94px 0 rgba(0, 0, 0, 0.1)',
+        'goods-card': '0 4px 94px 0 rgba(0, 0, 0, 0.1), 0 0 5px 0 rgba(0, 0, 0, 0.05)'
+      },
+      colors: {
+        blue: {
+          normal: '#262267'
+        },
+        gray: {
+          light: '#EEEEEE',
+          normal: '#BCBCBC',
+        }
+      },
+      screens: {
+        sm: '370px',
+        md: '687px',
+        lg: '1250px',
+        xl: '1750px',
+      },
+      container: {
+        center: true,
+        screens: {
+          sm: '370px',
+          md: '687px',
+          lg: '1250px',
+          xl: '1750px',
+        },
+      },
+    }
+  }
+}
